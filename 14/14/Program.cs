ff18779101a1e1258372b5777f33603637874914
adamgareb <92087000+adamgareb@users.noreply.github.com>
2022-09-08 01:08:12 +0200

    Add files via upload

---
+++ 14/14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me the truth!");
+            bool input = bool.Parse(Console.ReadLine());
+            Console.WriteLine("You gave " + input);
+        }
+    }
+}
