0cf8b313f2ccff68c5c8b5afa72f25c8153810eb
adamgareb <92087000+adamgareb@users.noreply.github.com>
2022-09-08 01:05:35 +0200

    Add files via upload

---
+++ 6/6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise_06
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Give input! \n>");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input);
+
+        }
+    }
+}
