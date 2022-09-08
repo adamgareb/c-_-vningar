9a22ba97fdc44b075f11545b0eff57f488cfaaa1
adamgareb <92087000+adamgareb@users.noreply.github.com>
2022-09-09 00:02:54 +0200

    Add files via upload

---
+++ 13/13/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a number");
+            string number1 = Console.ReadLine();
+            Console.WriteLine("You gave " + number1);
+
+        }
+    }
+}
