c4bd9d8049ba993cb7a165f06dd48a8ea23566f6
adamgareb <92087000+adamgareb@users.noreply.github.com>
2022-09-09 10:30:55 +0200

    Add files via upload

---
+++ 13/13/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a number");
+            double number1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("You gave " + number1);
+
+        }
+    }
+}
---
+++ 15/15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a string:");
+            string StringInput = Console.ReadLine();
+
+            Console.WriteLine("Give an interger:");
+            int IntInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give a double:");
+            double DoubleInput = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give a boolean:");
+            bool BooleanInput = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your string: " + StringInput);
+            Console.WriteLine("Your interger: " + IntInput);
+            Console.WriteLine("Your double: " + DoubleInput);
+            Console.WriteLine("Your boolean: " + BooleanInput);
+        }
+    }
+}
